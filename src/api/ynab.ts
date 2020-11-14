0909b8250d90f33c934d5a7d937a97f5dc5cca48
Rory Ksionda <ksiondag846@gmail.com>
2020-11-13 19:38:59 -0800

    Moving script to ynab api area, will break into FE-usable chunks

--- src/api/ynab.ts
+++ src/api/ynab.ts
@@ -62,3 +62,43 @@ export const budgetAccounts = async (): Promise<Array<Account>> => {
     const mainAccounts = await getMainAccounts();
     return mainAccounts.data.accounts.filter((a) => a.on_budget && (a.type === ynab.Account.TypeEnum.CreditCard) || a.name === "Chase Checking");
 };
+
+
+// TODO: Break this script up and make it an interactive FE setup, add the updates to the
+export const syncWithRealAccounts = async () => {
+    const budgets = await getBudgets();
+
+    const mainBudget = budgets.data.budgets.find((b) => b.name === 'My Budget');
+    const mainAccounts = await getAccounts(mainBudget.id);
+
+    const investmentBudget = budgets.data.budgets.find((b) => b.name === 'Investment Accounts');
+    const investmentAccounts = await getAccounts(investmentBudget.id);
+
+    const paidOffForLifeAccounts = mainAccounts.data.accounts.filter((a) => !a.closed && (a.type === ynab.Account.TypeEnum.OtherAsset || a.name === `Paid Off for Life` || a.name === `Index Budgeted`));
+    const paidOffForLifeTotal = paidOffForLifeAccounts.reduce((prev, a) => prev + a.balance, 0);
+    const investmentsTotal = investmentAccounts.data.accounts.filter((a) => a.type === ynab.Account.TypeEnum.OtherAsset).reduce((prev, a) => prev + a.balance, 0);
+    let delta = investmentsTotal - paidOffForLifeTotal;
+
+    if (delta === 0) {
+        return;
+    }
+
+    const interest = 1 - paidOffForLifeTotal/investmentsTotal;
+
+    // TODO: Save lost money in 'Paid Off for Life' and 'Index Budgeted' and explicitly have paid off for life accounts make up for lost funds
+    const transactions = paidOffForLifeAccounts.map((a) => {
+        const amount =  Math.round(a.balance * interest / 10) * 10;
+        delta -= amount;
+        return {
+            account_id: a.id,
+            date: "2020-11-12",
+            amount,
+            payee_name: "Market updates",
+            // cleared: "cleared",
+        };
+    });
+
+    transactions[0].amount += delta;
+
+    api.transactions.createTransactions(mainBudget.id, {transactions});
+};
