e515b99ba6093e0afd72dcba3b3336fdd91d630a
Rory Ksionda <ksiondag846@gmail.com>
2021-02-18 12:32:46 -0800

    (POC) (DIRTY) Automatic calculation of withdrawal amount

--- src/containers/Funds.tsx
+++ src/containers/Funds.tsx
@@ -19,7 +19,39 @@ export default function Funds() {
 
         // TODO: look at upcoming transactions and last month's direct bank transactions to come up with a buffer
         // Come up with rules for the different accounts
-        setSuperFluousFunds(budgetAccounts.reduce((prev, a) => prev + a.balance, 0) - 2500*1000 - 2000*1000)
+        setSuperFluousFunds(budgetAccounts.reduce((prev, a) => prev + a.balance, 0) - 2500 * 1000 - 2000 * 1000);
+    };
+
+    const withdrawalAmount = (balance: number, note: string) => {
+        const rules = (note ? note.split(`\n`) : []).filter(rule => rule.startsWith(`POFL: `));
+
+        let setAmount;
+        let inflate = false;
+        let deflate = false;
+        for (const index in rules) {
+            const rule = rules[index];
+
+            if (rule.search("Inflate") !== -1) {
+                inflate = true;
+            }
+
+            if (rule.search("Deflate") !== -1) {
+                deflate = true;
+            }
+
+            setAmount = setAmount ? setAmount : rule.match(/[0-9]+/);
+        }
+
+        if (setAmount) {
+            let newBalance = Number(setAmount) * 300000;
+            if (inflate) {
+                newBalance = Math.max(newBalance, balance);
+            } else if (deflate) {
+                newBalance = Math.min(newBalance, balance);
+            }
+            balance = newBalance;
+        }
+        return (Math.floor(balance / 3000) / 100).toLocaleString('en', { minimumFractionDigits: 2 });
     };
 
     return (
@@ -32,7 +64,7 @@ export default function Funds() {
                 <tbody>
                     <tr key="Superfluous funds">
                         <td>Superfluous funds</td>
-                        <td style={{textAlign: "right"}}>${(superfluousFunds/1000).toLocaleString('en', {minimumFractionDigits: 2})}</td>
+                        <td style={{ textAlign: "right" }}>${(superfluousFunds / 1000).toLocaleString('en', { minimumFractionDigits: 2 })}</td>
                     </tr>
                 </tbody>
             </Table>
@@ -40,15 +72,17 @@ export default function Funds() {
                 <thead>
                     <tr key="Funds header">
                         <th>Fund Name</th>
-                        <th style={{textAlign: "right"}}>Balance {funds.length > 0 ? `($${(funds.reduce((prev, {balance}) => prev + balance, 0)/1000).toLocaleString('en', {minimumFractionDigits: 2})})`: null}</th>
+                        <th>Monthly Withdrawal</th>
+                        <th style={{ textAlign: "right" }}>Balance {funds.length > 0 ? `($${(funds.reduce((prev, { balance }) => prev + balance, 0) / 1000).toLocaleString('en', { minimumFractionDigits: 2 })})` : null}</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        funds.map(({ id, name, balance }) => {
+                        funds.map(({ id, name, balance, note }) => {
                             return <tr key={id}>
                                 <td>{name}</td>
-                                <td style={{textAlign: "right"}}>${(balance/1000).toLocaleString('en', {minimumFractionDigits: 2})}</td>
+                                <td>{withdrawalAmount(balance, note)}</td>
+                                <td style={{ textAlign: "right" }}>${(balance / 1000).toLocaleString('en', { minimumFractionDigits: 2 })}</td>
                             </tr>
                         })
                     }
